94f9d477ca4b2c417d2881f9cd5555eeff5cc43a
CollinTift <collin.tift@gmail.com>
2024-10-05 20:01:20 -0400

    Add label and ability for new constellation

--- app/sky_view.gd
+++ app/sky_view.gd
@@ -4,9 +4,6 @@ extends Node3D
 const ApiHelper = preload("res://ApiHelper.gd")
 const StarGenerator = preload("res://StarGenerator.gd")
 
-func _process(_delta: float) -> void:
-	_update_mouse_line()
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	call_deferred("_init_mouse_line")
@@ -24,6 +21,15 @@ func _ready():
 
 	#$Stars.star_enter.connect(_on_star_enter)
 	$Stars.star_click.connect(_on_star_click)
+
+var newConstellation = false
+
+func _input(event: InputEvent):
+	if event is InputEventKey and event.pressed and event.keycode == KEY_BACKSPACE:
+		_clear_points_and_lines()
+	if event is InputEventKey and event.pressed and event.keycode == KEY_ENTER:
+		print("enter pressed")
+		newConstellation = true
 
 func _on_populate_stars(stars: Array[ApiHelper.StarData]):
 	print("Got a bunch of stars %d" % stars.size())
@@ -44,51 +50,27 @@ func _on_star_click(star: ApiHelper.StarData):
 	$StarLabel.text = "%f %f" % [star.ra, star.dec]
 	_draw_point_and_line(star)
 
-func _update_mouse_line():
-	var mouse_pos = get_mouse_pos()
-	var mouse_line_immediate_mesh = mouse_line.mesh as ImmediateMesh
-	if mouse_pos != null:
-		var mouse_pos_V3:Vector3 = mouse_pos
-		mouse_line_immediate_mesh.clear_surfaces()
-		mouse_line_immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES)
-		mouse_line_immediate_mesh.surface_add_vertex(Vector3.ZERO)
-		mouse_line_immediate_mesh.surface_add_vertex(mouse_pos_V3)
-		mouse_line_immediate_mesh.surface_end()	
-
-#Returns the position in 3d that the mouse is hovering, or null if it isnt hovering anything
-func get_mouse_pos():
-	var space_state = get_parent().get_world_3d().get_direct_space_state()
-	var mouse_position = get_viewport().get_mouse_position()
-	var camera = get_tree().root.get_camera_3d()
-	
-	var ray_origin = camera.project_ray_origin(mouse_position)
-	var ray_end = ray_origin + camera.project_ray_normal(mouse_position) * 1000
-		
-	var params = PhysicsRayQueryParameters3D.new()
-	params.from = ray_origin
-	params.to = ray_end
-	params.collision_mask = 1
-	params.exclude = []
-	
-	var rayDic = space_state.intersect_ray(params)	
-	
-	if rayDic.has("position"):
-		return rayDic["position"]
-	return null
-
 func _draw_point_and_line(star: ApiHelper.StarData):
-	print("click")
-	#var mouse_pos = get_mouse_pos()
-	#if mouse_pos != null:
-	#print("mouse")
-	#var mouse_pos_V3:Vector3 = mouse_pos
+	print(newConstellation)
 	var t = star.get_unit_direction() * 100
 	points.append(await Draw3D.point(t,0.05))
 	
 	#If there are at least 2 points...
-	if points.size() > 1:
+	if points.size() > 1 && newConstellation == false:
 		#Draw a line from the position of the last point placed to the position of the second to last point placed
 		var point1 = points[points.size()-1]
 		var point2 = points[points.size()-2]
 		var line = await Draw3D.line(point1.position, point2.position)
 		lines.append(line)
+
+	newConstellation = false
+
+func _clear_points_and_lines():
+	for p in points:
+		p.queue_free()
+	points.clear()
+		
+	for l in lines:
+		l.queue_free()
+	lines.clear()
+	newConstellation = true
